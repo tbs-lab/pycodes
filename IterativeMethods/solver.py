23e49a39de69d3931949e06f6498a923de0830a9
tbs-lab <tsubasa.i.0516@gmail.com>
2019-12-13 16:57:52 +0900

    [add] add cg_solve to Iterative methods

--- IterativeMethods/solver.py
+++ IterativeMethods/solver.py
@@ -10,7 +10,7 @@ def jacobi_solve(A, b, max_iterations):
 
     This algorithm refers to the folloing article:
         Fujio Kako
-        "数値解析: 4 連立方程式の求解", 2019,
+        "数値解析 4 連立方程式の求解", 2019,
         URL<https://www.ics.nara-wu.ac.jp/~kako/teaching/na/chap4.pdf>
 
     Arguments:
@@ -35,7 +35,7 @@ def jacobi_solve(A, b, max_iterations):
     # inverse diagonal vector of A
     dt = 1 / A.diagonal()
     # solution vector
-    x = numpy.ones(b.shape[0])
+    x = numpy.zeros(b.shape[0])
     # current number of iteration
     k = 0
     # residual 2-norm
@@ -57,7 +57,7 @@ def sor_solve(A, b, omega, max_iterations):
 
     This algorithm refers to the folloing article:
         Fujio Kako
-        "数値解析: 4 連立方程式の求解", 2019,
+        "数値解析 4 連立方程式の求解", 2019,
         URL<https://www.ics.nara-wu.ac.jp/~kako/teaching/na/chap4.pdf>
 
     Arguments:
@@ -89,7 +89,7 @@ def sor_solve(A, b, omega, max_iterations):
     # constant vector to use in iteration
     c = omega * dt * b
     # solution vector
-    x = numpy.ones(b.shape[0])
+    x = numpy.zeros(b.shape[0])
     # current number of iteration
     k = 0
     # residual infinity (maximum) norm
@@ -97,10 +97,63 @@ def sor_solve(A, b, omega, max_iterations):
 
     while k < max_iterations and residual >= sys.float_info.epsilon:
         _x = x
-        y = U @ x + c
+        y = U @ _x + c
         # forward substitution
         x = spla.solve_triangular(L, y, lower=True, unit_diagonal=True)
         residual = numpy.linalg.norm(x - _x, ord=numpy.inf)
+        k += 1
+
+    return x, k
+
+
+def cg_solve(A, b, max_iterations):
+    """Return a solution x of a linear system Ax = b with Conjugate-Gradient
+    Method.
+
+    This algorithm refers to the folloing article:
+        Kouya Tomonori
+        "ソフトウェアとしての数値計算 10 連立一次方程式の解法3 -- Krylov部分空間法", 2007,
+        URL<https://na-inet.jp/nasoft/chap10.pdf>
+
+    Arguments:
+        A (array_like): A square coefficient matrix.
+        b (array_like): A right-hand side vector.
+        max_iterations (int): A maximum number of iterations.
+
+    Returns:
+        numpy.ndarray: A solution of a linear system Ax = b.
+        int: A number of iterations.
+    """
+    A = numpy.array(A, dtype=numpy.float)
+    b = numpy.array(b, dtype=numpy.float)
+
+    if A.shape[0] != A.shape[1]:
+        raise ValueError("matrix must be square one")
+    if A.shape[0] != b.shape[0]:
+        raise ValueError("matrix and vector size must be aligned")
+
+    # solution vector
+    x = numpy.zeros(b.shape[0])
+    # residual vector
+    r = b - A @ x
+    # modification vector
+    p = r
+    # current number of iteration
+    k = 0
+    # residual infinity (maximum) norm`
+    residual = numpy.inf
+
+    while k < max_iterations:
+        alpha = numpy.dot(r, p) / numpy.dot(p, A @ p)
+        _x = x
+        x = _x + alpha * p
+        residual = numpy.linalg.norm(x - _x, ord=numpy.inf)
+        if residual < sys.float_info.epsilon:
+            break
+        _r = r
+        r = _r - alpha * A @ p
+        beta = (numpy.linalg.norm(r) / numpy.linalg.norm(_r)) ** 2
+        p = r + beta * p
         k += 1
 
     return x, k
